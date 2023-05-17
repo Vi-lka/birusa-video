69c6afea6734979876ff153dcbd1132bd3ca5fd4
Vi <vitaly.permyakov155@gmail.com>
2023-05-17 21:18:32 +0700

    lets start

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+function Home({ }: Props) {
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>Home</div>
+        </main>
+    )
+}
+
+export default Home
