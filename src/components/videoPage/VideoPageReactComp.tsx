de4c055f29da381a0582a1f573f70fe53d4aa8b8
vi <vitalya.permyakov155@gmail.com>
2023-05-24 14:44:20 +0700

    loading screen

--- src/components/videoPage/VideoPageReactComp.tsx
+++ src/components/videoPage/VideoPageReactComp.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { Suspense } from 'react'
 import Loading from '@/components/ui/loading'
 import { CONTENT, globalAutoplay } from '@/utils/content';
 import ReactPlayer from 'react-player';
@@ -32,77 +32,84 @@ export default function VideoPageReactComp() {
         }
     });
 
+    console.log(loading)
+
     return (
         <>
             {loading ? <Loading/> : null}
-            <div className="relative w-screen h-screen flex-col items-center justify-between" style={{display: loading ? 'none' : 'flex'}}>
-                <div 
-                    className='w-fit h-fit'
-                    onClick={() => {
-                        setPlay(!play)
-                        globalAutoplay.click = !play
-                    }}
-                >
-                    <ReactPlayer 
-                        style={{zIndex: 1}}
-                        width={'100vw'}
-                        height={'100vh'}
-                        playing={play} 
-                        url={"../video/" + CONTENT[currentVideo].filename + ".mp4"} 
-                        controls={false} 
-                        playsinline 
-                        stopOnUnmount={true} 
-                        loop={false} 
-                        preload={'auto'} 
-                        onReady={() => {
-                            setLoading(false)
-                        }}
-                        onEnded={() => {
-                            setEnded(true)
-                        }}
-                    />
-                </div>
-                <div className="absolute bottom-8 left-0 flex w-full justify-evenly">
-                {
-                    buttons.map((value, index) => 
-                        <button 
-                            key={index} 
-                            className='text-teal-300 bg-zinc-800 p-3 z-50'
-                            style={{display: ended ? 'block' : 'block'}}
-                            onClick={() => { 
-                                globalAutoplay.click = true
-                                setCurrentVideo(value.indexUrl)
-                            }} 
+            {CONTENT.map(element => (
+                element.id === currentVideo ? (
+                    <div key={element.id} className="relative w-screen h-screen flex-col items-center justify-between" style={{display: loading ? 'none' : 'flex'}}>
+                        <div 
+                            className='w-fit h-fit'
+                            onClick={() => {
+                                setPlay(!play)
+                                globalAutoplay.click = !play
+                            }}
                         >
-                            {value.name}
-                        </button>
-                    )
-                }
-                </div>
-                <div className="absolute inset-y-1/2 left-0 w-full justify-center" style={{display: play ? 'none' : 'flex'}}>
-                    <button 
-                        className='w-fit h-fit text-3xl text-teal-300 bg-zinc-800 p-3 z-50'                     
-                        onClick={() => {
-                            setPlay(!play)
-                            globalAutoplay.click = !play
-                        }}
-                    >
-                        Click to play
-                    </button>
-                </div>
-
-                <div className="absolute top-8 left-0 w-full flex justify-center">
-                    <button 
-                        className='w-fit h-fit text-xl text-teal-300 bg-zinc-800 p-2 z-50'                     
-                        onClick={() => {
-                            globalAutoplay.click = true
-                            setCurrentVideo(0)
-                        }}
-                    >
-                        back to start
-                    </button>
-                </div>
-            </div>
+                                    <ReactPlayer 
+                                        style={{zIndex: 1}}
+                                        width={'100vw'}
+                                        height={'100vh'}
+                                        playing={play} 
+                                        url={"../video/" + element.filename + ".mp4"} 
+                                        controls={false} 
+                                        playsinline 
+                                        stopOnUnmount={true} 
+                                        loop={false} 
+                                        preload={'auto'} 
+                                        onReady={() => {
+                                            setLoading(false)
+                                        }}
+                                        onEnded={() => {
+                                            setEnded(true)
+                                        }}
+                                    />
+                        </div>
+                        <div className="absolute bottom-8 left-0 flex w-full justify-evenly">
+                        {
+                            buttons.map((value, index) => 
+                                <button 
+                                    key={index} 
+                                    className='text-teal-300 bg-zinc-800 p-3 z-50'
+                                    style={{display: ended ? 'block' : 'block'}}
+                                    onClick={() => { 
+                                        globalAutoplay.click = true
+                                        setCurrentVideo(value.indexUrl)
+                                        setLoading(true)
+                                    }} 
+                                >
+                                    {value.name}
+                                </button>
+                            )
+                        }
+                        </div>
+                        <div className="absolute inset-y-1/2 left-0 w-full justify-center" style={{display: play ? 'none' : 'flex'}}>
+                            <button 
+                                className='w-fit h-fit text-3xl text-teal-300 bg-zinc-800 p-3 z-50'                     
+                                onClick={() => {
+                                    setPlay(!play)
+                                    globalAutoplay.click = !play
+                                }}
+                            >
+                                Click to play
+                            </button>
+                        </div>
+                            
+                        <div className="absolute top-8 left-0 w-full flex justify-center">
+                            <button 
+                                className='w-fit h-fit text-xl text-teal-300 bg-zinc-800 p-2 z-50'                     
+                                onClick={() => {
+                                    globalAutoplay.click = true
+                                    setCurrentVideo(0)
+                                }}
+                            >
+                                back to start
+                            </button>
+                        </div>
+                    </div>
+                ) : null
+            ))}
         </>
     )
 }
